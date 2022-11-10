a216971a33fb0ae17504092b9df16aa3e4ed849f
md-riaj11 <contact.riaj@gmail.com>
2022-11-10 22:17:01 +0600

    finishing server

--- index.js
+++ index.js
@@ -6,19 +6,19 @@ const { timeStamp } = require("./util/timeStamp");
 require("dotenv").config();
 
 const app = express();
+const port = process.env.PORT || 8000;
 
 app.use(express.json());
 app.use(cors());
 
-//server root directory
 app.get("/", (req, res) => {
   res.send("Server Working");
 });
 
+// middleware
 const verifyToken = (req, res, next) => {
   try {
     const authorization = req.headers.authorization;
-    console.log(authorization);
     if (!authorization) res.status(401).send("Unauthorized request");
 
     const accessToken = authorization.split(" ")[1];
@@ -39,14 +39,64 @@ async function run() {
     const client = new MongoClient(process.env.DB_URI);
     const database = await client.db("dentina");
 
+    //calling collections
     const serviceCollection = await database.collection("services");
     const reviewCollection = await database.collection("reviews");
 
+    /**
+     * Method: POST
+     */
+
+    // generate jwt token
+    app.post("/create-token", async (req, res) => {
+      const { email } = req.body;
+
+      if (!email) return res.status(400).send("Something went wrong !");
+      // create token
+      const token = jwt.sign({ email }, process.env.JWT_SECRET_KEY, {
+        expiresIn: "1d",
+      });
+
+      res.send({ token });
+    });
+
+    // add services
+    app.post("/add-service", verifyToken, async (req, res) => {
+      const service = req.body;
+
+      if (!service)
+        return res.status(400).send({ msg: "Service data is required !" });
+
+      const result = await serviceCollection.insertOne({
+        ...service,
+        createdAt: timeStamp(),
+      });
+
+      res.send(result);
+    });
+
+    // add review
+    app.post("/add-review", verifyToken, async (req, res) => {
+      const { serviceId, ...rest } = req.body;
+
+      const result = await reviewCollection.insertOne({
+        ...rest,
+        serviceId: ObjectId(serviceId),
+        createdAt: timeStamp(),
+      });
+
+      res.send(result);
+    });
+
+    /**
+     * Method: GET
+     */
     // all services and limit
     app.get("/services", async (req, res) => {
       let result = null;
       //checking limit
       const limit = parseInt(req?.query?.limit ?? 100000000000);
+
       result = await serviceCollection
         .find({})
         .limit(limit)
@@ -72,15 +122,18 @@ async function run() {
             (rating.rate.reduce((a, v) => a + v, 0) / rating.rate.length) * 2
           ) / 2;
 
-        collection.push({
+        const reviewObj = {
           ...rating,
           rate: avgRate ? avgRate : 0,
           totalRating: rating.rate.length,
-        });
+        };
+
+        collection.push(reviewObj);
       });
 
       res.send(collection);
     });
+
     //single service
     app.get("/service/:id", async (req, res) => {
       const id = req.params.id;
@@ -89,6 +142,7 @@ async function run() {
       // get reviews and
       const reviews = await reviewCollection.find().toArray();
       const rating = { ...service, rate: [] };
+
       reviews.forEach((review) => {
         if (service._id.toString() === review.serviceId.toString()) {
           rating.rate.push(review.rating);
@@ -100,24 +154,13 @@ async function run() {
           (rating.rate.reduce((a, v) => a + v, 0) / rating.rate.length) * 2
         ) / 2;
 
-      const result = {
+      const reviewObj = {
         ...rating,
         rate: avgRate ? avgRate : 0,
         totalRating: rating.rate.length,
       };
 
-      res.send(result);
-    });
-    // add services
-    app.post("/addservice", async (req, res) => {
-      const service = req.body;
-
-      const result = await serviceCollection.insertOne({
-        ...service,
-        createdAt: timeStamp(),
-      });
-
-      res.send(result);
+      res.send(reviewObj);
     });
 
     // all reviews list
@@ -129,38 +172,28 @@ async function run() {
 
       res.send(result);
     });
-    // singel service  review
+
+    // single service  review
     app.get("/review/:id", async (req, res) => {
-      const id = req.params.id;
+      const { id } = req.params;
+
       const result = await reviewCollection
         .find({ serviceId: ObjectId(id) })
         .sort({ createdAt: -1 })
         .toArray();
+
       res.send(result);
     });
 
-    // singel service  review
-    app.get("/reviewdetails/:id", async (req, res) => {
-      const id = req.params.id;
+    // single review
+    app.get("/review-details/:id", async (req, res) => {
+      const { id } = req.params;
       const result = await reviewCollection.findOne({ _id: ObjectId(id) });
       res.send(result);
     });
 
-    // add review
-    app.post("/review", verifyToken, async (req, res) => {
-      console.log(req.decoded);
-      const { serviceId, ...rest } = req.body;
-
-      const result = await reviewCollection.insertOne({
-        ...rest,
-        serviceId: ObjectId(serviceId),
-        createdAt: timeStamp(),
-      });
-      res.send(result);
-    });
-
     // my reviews
-    app.get("/myreview", verifyToken, async (req, res) => {
+    app.get("/my-review", verifyToken, async (req, res) => {
       // checking email is valid
       if (req?.decode?.email !== req.query.email) {
         res.status(401).send({ message: "Unauthorized Access" });
@@ -174,8 +207,12 @@ async function run() {
       res.send(result);
     });
 
+    /**
+     * Method: PUT
+     */
+
     // edit review
-    app.put("/myreview", verifyToken, async (req, res) => {
+    app.put("/update-my-review", verifyToken, async (req, res) => {
       const { _id, email, serviceId, ...updateDoc } = req.body;
       if (req?.decode?.email !== email) {
         res.status(401).send({ message: "Unauthorized Access" });
@@ -193,8 +230,12 @@ async function run() {
       res.send(result);
     });
 
+    /**
+     * Method: DELETE
+     */
+
     // delete review
-    app.delete("/myreview", verifyToken, async (req, res) => {
+    app.delete("/delete-my-review", verifyToken, async (req, res) => {
       const theReview = await reviewCollection.findOne({
         _id: ObjectId(req.body.id),
       });
@@ -209,26 +250,12 @@ async function run() {
       });
       res.send(result);
     });
-
-    // generate jwt token
-    app.post("/create-token", async (req, res) => {
-      const { email } = req.body;
-
-      console.log(email);
-      if (!email) return res.status(400).send("Something went wrong !");
-      // create token
-      const token = jwt.sign({ email }, process.env.JWT_SECRET_KEY, {
-        expiresIn: "1d",
-      });
-
-      res.send({ token });
-    });
   } finally {
   }
 }
 
-run();
+run().catch((error) => console.log(error));
 
-app.listen(5000, () => {
-  console.log("server listen on port 5000");
+app.listen(port, () => {
+  console.log(`server listen on port ${port}`);
 });
